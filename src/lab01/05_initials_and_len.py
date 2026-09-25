5a1bdd433507a83e255c0905878a98546027942e
kate <kattygromakova@gmail.com>
2026-09-25 11:10:22 +0300

    add code

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -2,7 +2,7 @@ fio = input("ФИО: ").split()
 init = ''
 kol = 0
 for i in fio:
-    init += i[0]
+    init += i[0].upper()
     kol += len(i)
 print(f"Инициалы: {init}")
 print(f"Длина (символов): {kol + len(init) - 1}")
